6c427e97982e1e8fa6f1c356b31d77c90c79d50e
usama <usamaaqeel98@gmail.com>
2023-03-13 12:13:59 +0000

    fetch request linked to searchba. button click prints out relevant information

--- src/index.js
+++ src/index.js
@@ -1,29 +1,32 @@
 import "./style.css";
-import gridLayout from "./layout";
+import setLayout from "./layout";
 import { WeatherFactory } from "./factory";
 
-gridLayout();
+setLayout();
 
 // We are going to have an input in a search-bar, whose value 
 // we will input to the factory function.
 
 // This location bit will be a by user input, but London
 // is a good placeholder for now.
+const searchBtn = document.getElementById("search-btn");
+let location = "";
 
-const location = "new york";
-
-async function fetchRequest(location) {
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + location + "&APPID=2bd361fd896a074e2c8c2fce72989c80", {
-    mode:"cors"
-  }).then((response) => {
-    return response.json()
-  }).then((response) => {
-    // we will save these in some sort of 
-    console.log(response.name)
-    console.log(response.main)
-  }).catch(() => {
-    throw("Error fetching weather data from OpenWeatherApi!")
-  });
+async function fetchWeather() {
+  fetch("https://api.openweathermap.org/data/2.5/weather?q=" + location + "&APPID=2bd361fd896a074e2c8c2fce72989c80", {
+  mode:"cors"
+}).then((response) => {
+  return response.json()
+}).then((response) => {
+  // we will save these in some sort of 
+  console.log(response.name)
+  console.log(response.main)
+}).catch(() => {
+  throw("Error fetching weather data from OpenWeatherApi!")
+});
 }
 
-fetchRequest(location);
+searchBtn.addEventListener("click", () => {
+  location = document.getElementById("search-bar").value;
+  fetchWeather(); 
+})
--- src/layout.js
+++ src/layout.js
@@ -1,9 +1,30 @@
-export default function gridLayout() {
+export default function setLayout() {
   const body = document.querySelector("body");
   const header = document.createElement("header");
+  const formElement = document.createElement("form"); 
+  const searchInput = document.createElement("input");
+  const searchLabel = document.createElement("label");
   
+  searchLabel.setAttribute ("for", "title");
+  searchLabel.textContent = "title:";
+  searchLabel.classList.add("title-element");
+  formElement.appendChild(searchLabel);
+
+  searchInput.setAttribute("type", "text");
+  searchInput.classList.add("title-element");
+  searchInput.classList.add("input-box");
+  searchInput.setAttribute("id", "search-bar");
+  formElement.appendChild(searchInput);
+
+  const searchBtn = document.createElement("button");
+  searchBtn.setAttribute("id", "search-btn");
+  searchBtn.setAttribute("type", "button");
+  searchBtn.textContent = "Check Weather";
+  formElement.appendChild(searchBtn);
+  header.textContent = "Wheather?";
+  
+  header.appendChild(formElement);
   header.classList.add("grid-container");
-  header.textContent = "hello, it worked";
   
   body.appendChild(header);
 }
